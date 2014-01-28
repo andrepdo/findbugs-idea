4eab379d62aefa26cc1ac1c2278a790b43ede6e5
reto.merz@gmail.com <reto.merz@gmail.com@44a26ecb-77bf-3937-1939-e0ce03f0277e>
2014-01-28 23:16:33 +0000

    * Cosmetic formatting

--- src/src/impl/org/twodividedbyzero/idea/findbugs/actions/BaseAnalyzeAction.java
+++ src/src/impl/org/twodividedbyzero/idea/findbugs/actions/BaseAnalyzeAction.java
@@ -74,221 +74,225 @@ public abstract class BaseAnalyzeAction extends BaseAction {
 
 	private static final Logger LOGGER = Logger.getInstance(BaseAction.class.getName());
 
-  @Override
-  public void actionPerformed(final AnActionEvent e) {
-    final DataContext dataContext = e.getDataContext();
-    final Project project = e.getData(PlatformDataKeys.PROJECT);
-    final Module module = e.getData(LangDataKeys.MODULE);
-    if (project == null) {
-      return;
-    }
-    AnalysisScope scope = getInspectionScope(dataContext);
-    final boolean rememberScope = e.getPlace().equals(ActionPlaces.MAIN_MENU);
-    final AnalysisUIOptions uiOptions = AnalysisUIOptions.getInstance(project);
-    final PsiElement element = LangDataKeys.PSI_ELEMENT.getData(dataContext);
-    final BaseAnalysisActionDialog dlg = new BaseAnalysisActionDialog(AnalysisScopeBundle.message("specify.analysis.scope", "FindBugs Analyze"),
-            AnalysisScopeBundle.message("analysis.scope.title", "Analyze"),
-            project,
-            scope,
-            module != null && scope.getScopeType() != AnalysisScope.MODULE ? getModuleNameInReadAction( module ) : null,
-            rememberScope, AnalysisUIOptions.getInstance(project), element){
-      @Override
-      @Nullable
-      protected JComponent getAdditionalActionSettings(final Project project) {
-        return BaseAnalyzeAction.this.getAdditionalActionSettings(project, this);
-      }
-
-      @Override
-      protected void doHelpAction() {
-        HelpManager.getInstance().invokeHelp(getHelpTopic());
-      }
-
-      @SuppressWarnings({"RCN_REDUNDANT_NULLCHECK_OF_NONNULL_VALUE"})
-	  @NotNull
-      @Override
-      protected Action[] createActions() {
-        return new Action[]{getOKAction(), getCancelAction(), getHelpAction()};
-      }
-    };
-    dlg.show();
-    if (!dlg.isOK()) {
-      canceled();
-      return;
-    }
-    final int oldScopeType = uiOptions.SCOPE_TYPE;
-    scope = dlg.getScope(uiOptions, scope, project, module);
-    if (!rememberScope){
-      uiOptions.SCOPE_TYPE = oldScopeType;
-    }
-    uiOptions.ANALYZE_TEST_SOURCES = dlg.isInspectTestSources();
-    FileDocumentManager.getInstance().saveAllDocuments();
-
-    analyze(project, scope);
-  }
-
-
-  @NonNls
-  protected String getHelpTopic() {
-    return "reference.dialogs.analyzeDependencies.scope";
-  }
-
-
-  protected void canceled() {
-  }
-
-
-  protected abstract void analyze(@NotNull Project project, AnalysisScope scope);
-
-
-  @edu.umd.cs.findbugs.annotations.SuppressWarnings({"SIC_INNER_SHOULD_BE_STATIC_ANON"})
-  protected final Iterable<String> findClasses(@NotNull final Project project, final AnalysisScope scope) {
-	final Collection<String> ret = new ArrayList<String>(256);
-    final PsiManager psiManager = PsiManager.getInstance(project);
-    psiManager.startBatchFilesProcessingMode();
-    try {
-		//noinspection AnonymousInnerClass
-		scope.accept(new PsiRecursiveElementVisitor() {
-		FileType fileType;
-		String path;
-		VirtualFile virtualFile;
-        @Override public void visitFile(final PsiFile file) {
-			fileType = file.getFileType();
-			if (StdFileTypes.JAVA.equals(fileType)) {
-				path = AnalyzeUtil.getOutputClassFilePathForJavaFile(file, project);
-				if (path != null) {
-					addPath(path);
-				} else {
-					LOGGER.warn("No output class path found for PsiFile: " + file);
-				}
-			} else if (StdFileTypes.CLASS.equals(fileType)) {
-				virtualFile = file.getVirtualFile();
-				if (virtualFile != null) {
-					path = virtualFile.getPath();
-					if (path != null) {
-						addPath(path);
-					} else {
-						LOGGER.warn("No path for VirtualFile: " + virtualFile);
-					}
-				} else {
-					LOGGER.warn("No virtual file for PsiFile: " + file);
-				}
-			} // else skip non .java and .class files
-        }
-		private void addPath(final String path) {
-			if (!ret.contains(path)) {
-				ret.add(path);
-			} // LATER: check why visitFile is called multiple times for the same PsiFile ?
+	@Override
+	public void actionPerformed(final AnActionEvent e) {
+		final DataContext dataContext = e.getDataContext();
+		final Project project = e.getData(PlatformDataKeys.PROJECT);
+		final Module module = e.getData(LangDataKeys.MODULE);
+		if (project == null) {
+			return;
 		}
-      });
-    } finally {
-      psiManager.finishBatchFilesProcessingMode();
-    }
-	return ret;
-  }
+		AnalysisScope scope = getInspectionScope(dataContext);
+		final boolean rememberScope = e.getPlace().equals(ActionPlaces.MAIN_MENU);
+		final AnalysisUIOptions uiOptions = AnalysisUIOptions.getInstance(project);
+		final PsiElement element = LangDataKeys.PSI_ELEMENT.getData(dataContext);
+		final BaseAnalysisActionDialog dlg = new BaseAnalysisActionDialog(AnalysisScopeBundle.message("specify.analysis.scope", "FindBugs Analyze"),
+			AnalysisScopeBundle.message("analysis.scope.title", "Analyze"),
+			project,
+			scope,
+			module != null && scope.getScopeType() != AnalysisScope.MODULE ? getModuleNameInReadAction(module) : null,
+			rememberScope, AnalysisUIOptions.getInstance(project), element) {
+				@Override
+				@Nullable
+				protected JComponent getAdditionalActionSettings(final Project project) {
+					return BaseAnalyzeAction.this.getAdditionalActionSettings(project, this);
+				}
+
+				@Override
+				protected void doHelpAction() {
+					HelpManager.getInstance().invokeHelp(getHelpTopic());
+				}
+
+				@SuppressWarnings({"RCN_REDUNDANT_NULLCHECK_OF_NONNULL_VALUE"})
+				@NotNull
+				@Override
+				protected Action[] createActions() {
+					return new Action[]{getOKAction(), getCancelAction(), getHelpAction()};
+				}
+			};
+		dlg.show();
+		if (!dlg.isOK()) {
+			canceled();
+			return;
+		}
+		final int oldScopeType = uiOptions.SCOPE_TYPE;
+		scope = dlg.getScope(uiOptions, scope, project, module);
+		if (!rememberScope) {
+			uiOptions.SCOPE_TYPE = oldScopeType;
+		}
+		uiOptions.ANALYZE_TEST_SOURCES = dlg.isInspectTestSources();
+		FileDocumentManager.getInstance().saveAllDocuments();
+
+		analyze(project, scope);
+	}
 
 
-  @Nullable
-  private AnalysisScope getInspectionScope(@NotNull final DataContext dataContext) {
-    if (PlatformDataKeys.PROJECT.getData(dataContext) == null) return null;
-
-    final AnalysisScope scope = getInspectionScopeImpl(dataContext);
-
-    return scope != null && scope.getScopeType() != AnalysisScope.INVALID ? scope : null;
-  }
+	@NonNls
+	protected String getHelpTopic() {
+		return "reference.dialogs.analyzeDependencies.scope";
+	}
 
 
-  @Nullable
-  private AnalysisScope getInspectionScopeImpl(@NotNull final DataContext dataContext) {
-    //Possible scopes: file, directory, package, project, module.
-    final Project projectContext = PlatformDataKeys.PROJECT_CONTEXT.getData(dataContext);
-    if (projectContext != null) {
-      return new AnalysisScope(projectContext);
-    }
-
-    final AnalysisScope analysisScope = AnalyzeUtil.KEY.getData(dataContext);
-    if (analysisScope != null) {
-      return analysisScope;
-    }
-
-    final Module moduleContext = LangDataKeys.MODULE_CONTEXT.getData(dataContext);
-    if (moduleContext != null) {
-      return new AnalysisScope(moduleContext);
-    }
-
-    final Module [] modulesArray = LangDataKeys.MODULE_CONTEXT_ARRAY.getData(dataContext);
-    if (modulesArray != null) {
-      return new AnalysisScope(modulesArray);
-    }
-    final PsiFile psiFile = LangDataKeys.PSI_FILE.getData(dataContext);
-    if (psiFile != null && psiFile.getManager().isInProject(psiFile)) {
-      final VirtualFile file = psiFile.getVirtualFile();
-      if (file != null && file.isValid() && file.getFileType() instanceof ArchiveFileType && acceptNonProjectDirectories()) {
-        final VirtualFile jarRoot = JarFileSystem.getInstance().getJarRootForLocalFile(file);
-        if (jarRoot != null) {
-          final PsiDirectory psiDirectory = psiFile.getManager().findDirectory(jarRoot);
-          if (psiDirectory != null) {
-            return new AnalysisScope(psiDirectory);
-          }
-        }
-      }
-      return new AnalysisScope(psiFile);
-    }
-
-    final VirtualFile[] virtualFiles = PlatformDataKeys.VIRTUAL_FILE_ARRAY.getData(dataContext);
-    final Project project = PlatformDataKeys.PROJECT.getData(dataContext);
-    if (virtualFiles != null && project != null) { //analyze on selection
-      final ProjectFileIndex fileIndex = ProjectRootManager.getInstance(project).getFileIndex();
-      if (virtualFiles.length == 1) {
-        final PsiDirectory psiDirectory = PsiManager.getInstance(project).findDirectory(virtualFiles[0]);
-        if (psiDirectory != null && (acceptNonProjectDirectories() || psiDirectory.getManager().isInProject(psiDirectory))) {
-          return new AnalysisScope(psiDirectory);
-        }
-      }
-      final Set<VirtualFile> files = new HashSet<VirtualFile>();
-      for (VirtualFile vFile : virtualFiles) {
-        if (fileIndex.isInContent(vFile)) {
-          if (vFile instanceof VirtualFileWindow) {
-            files.add(vFile);
-            vFile = ((VirtualFileWindow)vFile).getDelegate();
-          }
-          collectFilesUnder(vFile, files);
-        }
-      }
-      return new AnalysisScope(project, files);
-    }
-    return project == null ? null : new AnalysisScope(project);
-  }
+	protected void canceled() {
+	}
 
 
-  protected boolean acceptNonProjectDirectories() {
-    return false;
-  }
+	protected abstract void analyze(@NotNull Project project, AnalysisScope scope);
 
 
-  @Nullable
-  protected JComponent getAdditionalActionSettings(final Project project, final BaseAnalysisActionDialog dialog){
-    return null;
-  }
+	@edu.umd.cs.findbugs.annotations.SuppressWarnings({"SIC_INNER_SHOULD_BE_STATIC_ANON"})
+	protected final Iterable<String> findClasses(@NotNull final Project project, final AnalysisScope scope) {
+		final Collection<String> ret = new ArrayList<String>(256);
+		final PsiManager psiManager = PsiManager.getInstance(project);
+		psiManager.startBatchFilesProcessingMode();
+		try {
+			//noinspection AnonymousInnerClass
+			scope.accept(new PsiRecursiveElementVisitor() {
+
+				FileType fileType;
+				String path;
+				VirtualFile virtualFile;
+
+				@Override
+				public void visitFile(final PsiFile file) {
+					fileType = file.getFileType();
+					if (StdFileTypes.JAVA.equals(fileType)) {
+						path = AnalyzeUtil.getOutputClassFilePathForJavaFile(file, project);
+						if (path != null) {
+							addPath(path);
+						} else {
+							LOGGER.warn("No output class path found for PsiFile: " + file);
+						}
+					} else if (StdFileTypes.CLASS.equals(fileType)) {
+						virtualFile = file.getVirtualFile();
+						if (virtualFile != null) {
+							path = virtualFile.getPath();
+							if (path != null) {
+								addPath(path);
+							} else {
+								LOGGER.warn("No path for VirtualFile: " + virtualFile);
+							}
+						} else {
+							LOGGER.warn("No virtual file for PsiFile: " + file);
+						}
+					} // else skip non .java and .class files
+				}
+
+				private void addPath(final String path) {
+					if (!ret.contains(path)) {
+						ret.add(path);
+					} // LATER: check why visitFile is called multiple times for the same PsiFile ?
+				}
+			});
+		} finally {
+			psiManager.finishBatchFilesProcessingMode();
+		}
+		return ret;
+	}
 
 
-  private static void collectFilesUnder(@NotNull final VirtualFile vFile, @NotNull final Collection<VirtualFile> files) {
-    VfsUtilCore.visitChildrenRecursively(vFile, new VirtualFileVisitor() {
-      @Override
-      public boolean visitFile(@NotNull final VirtualFile file) {
-        if (!file.isDirectory()) {
-          files.add(file);
-        }
-        return true;
-      }
-    });
-  }
+	@Nullable
+	private AnalysisScope getInspectionScope(@NotNull final DataContext dataContext) {
+		if (PlatformDataKeys.PROJECT.getData(dataContext) == null) return null;
+
+		final AnalysisScope scope = getInspectionScopeImpl(dataContext);
+
+		return scope != null && scope.getScopeType() != AnalysisScope.INVALID ? scope : null;
+	}
 
 
-  private static String getModuleNameInReadAction(@NotNull final Module module) {
-    return new ReadAction<String>(){
-      protected void run(final Result<String> result) throws Throwable {
-        result.setResult(module.getName());
-      }
-    }.execute().getResultObject();
-  }
+	@Nullable
+	private AnalysisScope getInspectionScopeImpl(@NotNull final DataContext dataContext) {
+		//Possible scopes: file, directory, package, project, module.
+		final Project projectContext = PlatformDataKeys.PROJECT_CONTEXT.getData(dataContext);
+		if (projectContext != null) {
+			return new AnalysisScope(projectContext);
+		}
+
+		final AnalysisScope analysisScope = AnalyzeUtil.KEY.getData(dataContext);
+		if (analysisScope != null) {
+			return analysisScope;
+		}
+
+		final Module moduleContext = LangDataKeys.MODULE_CONTEXT.getData(dataContext);
+		if (moduleContext != null) {
+			return new AnalysisScope(moduleContext);
+		}
+
+		final Module [] modulesArray = LangDataKeys.MODULE_CONTEXT_ARRAY.getData(dataContext);
+		if (modulesArray != null) {
+			return new AnalysisScope(modulesArray);
+		}
+		final PsiFile psiFile = LangDataKeys.PSI_FILE.getData(dataContext);
+		if (psiFile != null && psiFile.getManager().isInProject(psiFile)) {
+			final VirtualFile file = psiFile.getVirtualFile();
+			if (file != null && file.isValid() && file.getFileType() instanceof ArchiveFileType && acceptNonProjectDirectories()) {
+				final VirtualFile jarRoot = JarFileSystem.getInstance().getJarRootForLocalFile(file);
+				if (jarRoot != null) {
+					final PsiDirectory psiDirectory = psiFile.getManager().findDirectory(jarRoot);
+					if (psiDirectory != null) {
+						return new AnalysisScope(psiDirectory);
+					}
+				}
+			}
+			return new AnalysisScope(psiFile);
+		}
+
+		final VirtualFile[] virtualFiles = PlatformDataKeys.VIRTUAL_FILE_ARRAY.getData(dataContext);
+		final Project project = PlatformDataKeys.PROJECT.getData(dataContext);
+		if (virtualFiles != null && project != null) { //analyze on selection
+			final ProjectFileIndex fileIndex = ProjectRootManager.getInstance(project).getFileIndex();
+			if (virtualFiles.length == 1) {
+				final PsiDirectory psiDirectory = PsiManager.getInstance(project).findDirectory(virtualFiles[0]);
+				if (psiDirectory != null && (acceptNonProjectDirectories() || psiDirectory.getManager().isInProject(psiDirectory))) {
+					return new AnalysisScope(psiDirectory);
+				}
+			}
+			final Set<VirtualFile> files = new HashSet<VirtualFile>();
+			for (VirtualFile vFile : virtualFiles) {
+				if (fileIndex.isInContent(vFile)) {
+					if (vFile instanceof VirtualFileWindow) {
+						files.add(vFile);
+						vFile = ((VirtualFileWindow)vFile).getDelegate();
+					}
+					collectFilesUnder(vFile, files);
+				}
+			}
+			return new AnalysisScope(project, files);
+		}
+		return project == null ? null : new AnalysisScope(project);
+	}
+
+
+	protected boolean acceptNonProjectDirectories() {
+		return false;
+	}
+
+
+	@Nullable
+	protected JComponent getAdditionalActionSettings(final Project project, final BaseAnalysisActionDialog dialog) {
+		return null;
+	}
+
+
+	private static void collectFilesUnder(@NotNull final VirtualFile vFile, @NotNull final Collection<VirtualFile> files) {
+		VfsUtilCore.visitChildrenRecursively(vFile, new VirtualFileVisitor() {
+			@Override
+			public boolean visitFile(@NotNull final VirtualFile file) {
+				if (!file.isDirectory()) {
+					files.add(file);
+				}
+				return true;
+			}
+		});
+	}
+
+
+	private static String getModuleNameInReadAction(@NotNull final Module module) {
+		return new ReadAction<String>() {
+			protected void run(final Result<String> result) throws Throwable {
+				result.setResult(module.getName());
+			}
+		}.execute().getResultObject();
+	}
 }
