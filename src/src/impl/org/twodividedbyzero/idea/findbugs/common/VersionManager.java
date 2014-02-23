376b20e8cfbff950f54239df4f4acdf82a281088
reto.merz@gmail.com <reto.merz@gmail.com@44a26ecb-77bf-3937-1939-e0ce03f0277e>
2014-02-23 17:26:54 +0000

    prepare 0.9.992 release

--- src/src/impl/org/twodividedbyzero/idea/findbugs/common/VersionManager.java
+++ src/src/impl/org/twodividedbyzero/idea/findbugs/common/VersionManager.java
@@ -41,7 +41,7 @@ public class VersionManager {
 
 	private static final long _major = 0;
 	private static final long _minor = 9;
-	private static final long _build = 991;
+	private static final long _build = 992;
 
 	private static final String _branch = "trunk";
 	private static final String rev = "$Revision$";
