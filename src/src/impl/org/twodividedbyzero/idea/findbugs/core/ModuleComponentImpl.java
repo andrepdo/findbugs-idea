9cf936727a77bec22abdc13189cd3de91145b1fc
reto.merz@gmail.com <reto.merz@gmail.com@44a26ecb-77bf-3937-1939-e0ce03f0277e>
2014-06-19 23:46:33 +0000

    comment

--- src/src/impl/org/twodividedbyzero/idea/findbugs/core/ModuleComponentImpl.java
+++ src/src/impl/org/twodividedbyzero/idea/findbugs/core/ModuleComponentImpl.java
@@ -78,7 +78,7 @@ public class ModuleComponentImpl implements FindBugsPlugin, ModuleComponent, Con
 	public ModuleComponentImpl(final Project project, final Module module) {
 		_project = project;
 		_module = module;
-		_preferences = FindBugsPreferences.createDefault(false);
+		_preferences = FindBugsPreferences.createDefault(false /* do not load plugins because custom plugins per module is not supported */);
 	}
 
 
