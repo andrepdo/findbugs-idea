8a4720baaf04b2a7234f802455721064026eeed3
reto.merz@gmail.com <reto.merz@gmail.com@44a26ecb-77bf-3937-1939-e0ce03f0277e>
2015-02-24 16:58:13 +0000

    make GroupBy actions stateless

---
+++ src/src/impl/org/twodividedbyzero/idea/findbugs/actions/AbstractGroupByAction.java
@@ -0,0 +1,98 @@
+/*
+ * Copyright 2008-2015 Andre Pfeiler
+ *
+ * This file is part of FindBugs-IDEA.
+ *
+ * FindBugs-IDEA is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * FindBugs-IDEA is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with FindBugs-IDEA.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package org.twodividedbyzero.idea.findbugs.actions;
+
+
+import com.intellij.openapi.actionSystem.AnActionEvent;
+import com.intellij.openapi.module.Module;
+import com.intellij.openapi.project.Project;
+import com.intellij.openapi.wm.ToolWindow;
+import com.intellij.ui.content.Content;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+import org.twodividedbyzero.idea.findbugs.core.FindBugsPlugin;
+import org.twodividedbyzero.idea.findbugs.core.FindBugsState;
+import org.twodividedbyzero.idea.findbugs.gui.toolwindow.view.ToolWindowPanel;
+import org.twodividedbyzero.idea.findbugs.gui.tree.GroupBy;
+import org.twodividedbyzero.idea.findbugs.preferences.FindBugsPreferences;
+
+import java.util.Arrays;
+
+
+/**
+ * @author $Author: reto.merz@gmail.com $
+ * @version $Revision: 384 $
+ * @since 0.9.995
+ */
+abstract class AbstractGroupByAction extends AbstractToggleAction {
+
+	private final GroupBy _groupBy;
+
+
+	AbstractGroupByAction(@NotNull final GroupBy groupBy) {
+		_groupBy = groupBy;
+	}
+
+	@Override
+	final boolean isSelectedImpl(
+			@NotNull final AnActionEvent e,
+			@NotNull final Project project,
+			@Nullable final Module module,
+			@NotNull final FindBugsPlugin plugin,
+			@NotNull final ToolWindow toolWindow,
+			@NotNull final FindBugsState state,
+			@NotNull final FindBugsPreferences preferences) {
+
+		final Content content = toolWindow.getContentManager().getContent(0);
+		if (content != null) {
+			final String groupByProperty = preferences.getProperty(FindBugsPreferences.TOOLWINDOW_GROUP_BY, GroupBy.BugCategory.name());
+			final boolean equals = _groupBy.name().equals(groupByProperty);
+			final ToolWindowPanel panel = (ToolWindowPanel) content.getComponent();
+			final GroupBy[] sortOrderGroup = GroupBy.getSortOrderGroup(_groupBy);
+			if(equals && !Arrays.equals(panel.getBugTreePanel().getGroupBy(), sortOrderGroup)) {
+				panel.getBugTreePanel().setGroupBy(sortOrderGroup);
+			}
+			return equals;
+		}
+		return false;
+	}
+
+
+	@Override
+	final void setSelectedImpl(
+			@NotNull final AnActionEvent e,
+			@NotNull final Project project,
+			@Nullable final Module module,
+			@NotNull final FindBugsPlugin plugin,
+			@NotNull final ToolWindow toolWindow,
+			@NotNull final FindBugsState state,
+			@NotNull final FindBugsPreferences preferences,
+			final boolean select) {
+
+		final Content content = toolWindow.getContentManager().getContent(0);
+		if (content != null) {
+			final ToolWindowPanel panel = (ToolWindowPanel) content.getComponent();
+			if (select) {
+				preferences.setProperty(FindBugsPreferences.TOOLWINDOW_GROUP_BY, _groupBy.name());
+				panel.getBugTreePanel().setGroupBy(GroupBy.getSortOrderGroup(_groupBy));
+			}
+		}
+	}
+}
--- src/src/impl/org/twodividedbyzero/idea/findbugs/actions/GroupByRank.java
+++ src/src/impl/org/twodividedbyzero/idea/findbugs/actions/GroupByRank.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2008-2013 Andre Pfeiler
+ * Copyright 2008-2015 Andre Pfeiler
  *
  * This file is part of FindBugs-IDEA.
  *
@@ -18,23 +18,8 @@
  */
 package org.twodividedbyzero.idea.findbugs.actions;
 
-import com.intellij.openapi.actionSystem.AnActionEvent;
-import com.intellij.openapi.actionSystem.DataKeys;
-import com.intellij.openapi.project.Project;
-import com.intellij.openapi.wm.ToolWindow;
-import com.intellij.openapi.wm.ToolWindowManager;
-import com.intellij.ui.content.Content;
-import org.jetbrains.annotations.NotNull;
-import org.twodividedbyzero.idea.findbugs.common.event.EventListener;
-import org.twodividedbyzero.idea.findbugs.common.event.EventManagerImpl;
-import org.twodividedbyzero.idea.findbugs.common.event.filters.BugReporterEventFilter;
-import org.twodividedbyzero.idea.findbugs.common.event.types.BugReporterEvent;
-import org.twodividedbyzero.idea.findbugs.core.FindBugsPlugin;
-import org.twodividedbyzero.idea.findbugs.gui.toolwindow.view.ToolWindowPanel;
-import org.twodividedbyzero.idea.findbugs.gui.tree.GroupBy;
-import org.twodividedbyzero.idea.findbugs.preferences.FindBugsPreferences;
 
-import java.util.Arrays;
+import org.twodividedbyzero.idea.findbugs.gui.tree.GroupBy;
 
 
 /**
@@ -44,109 +29,8 @@ import java.util.Arrays;
  * @version $Revision$
  * @since 0.9.9
  */
-public class GroupByRank extends BaseToggleAction implements EventListener<BugReporterEvent> {
-
-
-	@Override
-	public boolean isSelected(final AnActionEvent event) {
-		final Project project = DataKeys.PROJECT.getData(event.getDataContext());
-		if (project == null) {
-			return false;
-		}
-
-		final FindBugsPlugin findBugsPlugin = project.getComponent(FindBugsPlugin.class);
-		if (findBugsPlugin == null) {
-			throw new IllegalStateException("Couldn't get findbugs plugin");
-		}
-
-		final ToolWindow toolWindow = ToolWindowManager.getInstance(project).getToolWindow(getPluginInterface(project).getInternalToolWindowId());
-		registerEventListener(project);
-
-		// toggle value
-		final Content content = toolWindow.getContentManager().getContent(0);
-		if (content != null) {
-			final FindBugsPreferences preferences = getPluginInterface(project).getPreferences();
-			final String groupByProperty = preferences.getProperty(FindBugsPreferences.TOOLWINDOW_GROUP_BY, GroupBy.BugCategory.name());
-
-			final boolean equals = GroupBy.BugRank.name().equals(groupByProperty);
-			final ToolWindowPanel panel = (ToolWindowPanel) content.getComponent();
-			final GroupBy[] sortOrderGroup = GroupBy.getSortOrderGroup(GroupBy.BugRank);
-			if(equals && !Arrays.equals(panel.getBugTreePanel().getGroupBy(), sortOrderGroup)) {
-				panel.getBugTreePanel().setGroupBy(sortOrderGroup);
-			}
-			return equals;
-		}
-
-		return false;
+public final class GroupByRank extends AbstractGroupByAction {
+	public GroupByRank() {
+		super(GroupBy.BugRank);
 	}
-
-
-	@Override
-	public void setSelected(final AnActionEvent event, final boolean selected) {
-		final Project project = DataKeys.PROJECT.getData(event.getDataContext());
-		if (project == null) {
-			return;
-		}
-
-		final FindBugsPlugin findBugsPlugin = project.getComponent(FindBugsPlugin.class);
-		if (findBugsPlugin == null) {
-			throw new IllegalStateException("Couldn't get findbugs plugin");
-		}
-
-		final ToolWindow toolWindow = ToolWindowManager.getInstance(project).getToolWindow(getPluginInterface(project).getInternalToolWindowId());
-
-		// toggle value
-		final Content content = toolWindow.getContentManager().getContent(0);
-		if (content != null) {
-			final ToolWindowPanel panel = (ToolWindowPanel) content.getComponent();
-			final FindBugsPreferences preferences = getPluginInterface(project).getPreferences();
-			if (selected) {
-				preferences.setProperty(FindBugsPreferences.TOOLWINDOW_GROUP_BY, GroupBy.BugRank.name());
-				panel.getBugTreePanel().setGroupBy(GroupBy.getSortOrderGroup(GroupBy.BugRank));
-			}
-		}
-	}
-
-
-	private void registerEventListener(final Project project) {
-		final String projectName = project.getName();
-		if (!isRegistered(projectName)) {
-			EventManagerImpl.getInstance().addEventListener(new BugReporterEventFilter(projectName), this);
-			addRegisteredProject(projectName);
-		}
-	}
-
-
-	protected boolean setEnabled(final boolean enabled) {
-		final boolean was = _enabled;
-		if (_enabled != enabled) {
-			_enabled = enabled;
-		}
-		return was;
-	}
-
-
-	protected boolean setRunning(final boolean running) {
-		final boolean was = _running;
-		if (_running != running) {
-			_running = running;
-		}
-		return was;
-	}
-
-
-	public void onEvent(@NotNull final BugReporterEvent event) {
-		switch (event.getOperation()) {
-			case ANALYSIS_STARTED:
-				setEnabled(false);
-				setRunning(true);
-				break;
-			case ANALYSIS_ABORTED:
-			case ANALYSIS_FINISHED:
-				setEnabled(true);
-				setRunning(false);
-				break;
-		}
-	}
-
 }
