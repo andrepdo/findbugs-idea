2091875d68afdf2a1ee3d6c6aa76adadb4fd0477
Reto Merz <reto.merz@gmail.com>
2016-05-25 20:43:25 +0200

    Update build scripts.

--- src/setenv.bat
+++ src/setenv.bat
@@ -6,8 +6,7 @@ rem ****************************************
 echo Setting up environment ...
 
 set ANT_OPTS=-Xmx512m
-set JAVA_HOME=C:\Program Files\Java\jdk1.6.0_25
-REM set JAVA_HOME=C:\Program Files\Java\jdk1.8.0_77
+set JAVA_HOME=C:\Program Files\Java\jdk1.8.0_92
 set ANT_HOME=C:\tools\apache-ant-1.9.1
 set PATH=%JAVA_HOME%\bin;%PATH%;%CD%\bin;%ANT_HOME%\bin
 
