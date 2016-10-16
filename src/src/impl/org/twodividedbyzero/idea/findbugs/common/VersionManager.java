840391db3fe549c3ca8a81b0b940da8542cfb9cc
Reto Merz <reto.merz@gmail.com>
2016-10-16 20:27:52 +0200

    1.0.1

--- src/src/impl/org/twodividedbyzero/idea/findbugs/common/VersionManager.java
+++ src/src/impl/org/twodividedbyzero/idea/findbugs/common/VersionManager.java
@@ -41,7 +41,7 @@ public class VersionManager {
 
 	private static final long _major = 1;
 	private static final long _minor = 0;
-	private static final long _build = 0;
+	private static final long _build = 1;
 
 	private static final String _branch = "trunk";
 
